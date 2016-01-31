99adb7cc3979410b56cf0729a9f0553962716113
Stanislav <ch3rkasov@gmail.com>
2016-01-31 15:34:05 +0300

    sql

--- epicrp.sql
+++ epicrp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Янв 17 2016 г., 16:09
+-- Время создания: Янв 31 2016 г., 16:33
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -39,7 +39,9 @@ CREATE TABLE IF NOT EXISTS `players` (
   `warns` int(11) NOT NULL,
   `sex` int(11) NOT NULL,
   `exp` int(11) NOT NULL,
-  `lvl` int(11) NOT NULL,
+  `lvl` int(11) NOT NULL DEFAULT '1',
+  `money` float NOT NULL,
+  `donate` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
@@ -47,8 +49,38 @@ CREATE TABLE IF NOT EXISTS `players` (
 -- Дамп данных таблицы `players`
 --
 
-INSERT INTO `players` (`id`, `name`, `pass`, `email`, `banned`, `muted`, `kicks`, `bans`, `mutes`, `warns`, `sex`, `exp`, `lvl`) VALUES
-(1, 'Stanislav_Cherkasov', 'FFA5BCA9496243922131D1C65DA51F0C', 'ch3rkasov@gmail.com', 0, 0, 0, 0, 0, 0, 0, 0, 1);
+INSERT INTO `players` (`id`, `name`, `pass`, `email`, `banned`, `muted`, `kicks`, `bans`, `mutes`, `warns`, `sex`, `exp`, `lvl`, `money`, `donate`) VALUES
+(1, 'Stanislav_Cherkasov', 'FFA5BCA9496243922131D1C65DA51F0C', 'ch3rkasov@gmail.com', 0, 0, 10, 0, 0, 0, 0, 0, 1, 0, 1488);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rights`
+--
+
+CREATE TABLE IF NOT EXISTS `rights` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerid` int(11) NOT NULL,
+  `rightid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Дамп данных таблицы `rights`
+--
+
+INSERT INTO `rights` (`id`, `ownerid`, `rightid`) VALUES
+(1, 1, 0),
+(5, 1, 26),
+(6, 1, 1),
+(7, 1, 2),
+(8, 1, 3),
+(9, 1, 4),
+(10, 1, 5),
+(11, 1, 6),
+(12, 1, 7),
+(13, 1, 8),
+(14, 1, 9);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
